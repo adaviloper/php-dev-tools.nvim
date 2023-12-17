26a044ebed8db54336b99f8b6c965f49009d64cb
adaviloper <16654653+adaviloper@users.noreply.github.com>
2023-12-17 15:16:14 -0600

    fixed parsing logic

--- lua/php-symbolize/init.lua
+++ lua/php-symbolize/init.lua
@@ -1,63 +1,118 @@
-local M = {}
-
--- local p = function (message)
---   vim.notify(message, 3)
--- end
-
 local ts = vim.treesitter
 local parsers = require('nvim-treesitter.parsers')
+local parser = parsers.get_parser()
+local targetClass = nil
+local targetMethod = nil
 
+local imported_name_qs = [[
+(array_creation_expression
+  (array_element_initializer
+    (class_constant_access_expression
+      (name)? @class (#not-eq? @class "class")
+    )
+  )
+  (array_element_initializer
+    (string (string_value) @method)
+  )
+)
+]]
+local qualified_name_qs = [[
+(array_creation_expression
+  (array_element_initializer
+    (class_constant_access_expression
+      (qualified_name
+        (name) @class
+      )
+    )
+  )
+  (array_element_initializer
+    (string (string_value) @method)
+  )
+)
+  ]]
 
+local M = {}
 
----@return boolean, TSNode?, TSNode?
+local p = function(message)
+  vim.notify(message, 3)
+end
+
+local function get_namespace(class_name)
+  local language = parser:lang()
+  local tree = parser:parse()[1]
+  local node = tree:root()
+  local namespace_qs = '\
+(namespace_use_declaration\
+  (namespace_use_clause\
+    (qualified_name) @class (#match? @class ".*' .. class_name .. '")\
+    )\
+  )\
+'
+  local query = ts.query.parse(language, namespace_qs)
+  local namespace = nil
+  for i, match, _ in query:iter_captures(node, 0) do
+    local name = query.captures[i]
+    if name == 'class' then
+      return ts.get_node_text(match, 0)
+    end
+  end
+end
+
+local function parse_groups(query, current_node, declaration_type)
+  for i, match, _ in query:iter_captures(current_node:parent():parent(), 0) do
+    local name = query.captures[i]
+    if name == 'method' then
+      targetMethod = ts.get_node_text(match, 0)
+    end
+    if name == 'class' then
+      targetClass = ts.get_node_text(match, 0)
+    end
+  end
+end
+
+---@return boolean, string?, string?, string?
 M.validate_touple = function()
-  local node = ts.get_node()
-  if node == nil then
+  local language = parser:lang()
+  if language ~= 'php' then
     return false
   end
-  local parser = parsers.get_parser()
-  local tree = parser:parse()[1]
-  local root = tree:root()
-  local qs = [[
-(array_creation_expression
-  (array_element_initializer (class_constant_access_expression)) @class
-  (array_element_initializer (string)) @method
-  ) @array
-]]
-  local language = parser:lang()
+  local node = ts.get_node()
+  if node == nil or (node:type() ~= 'string' and node:type() ~= 'string_value') then
+    return false
+  end
+  if node:type() == 'string_value' then
+    node = node:parent()
+  end
+  local sibling_type = node:parent():prev_sibling():prev_sibling():child(0):child(0):type()
 
-  ts.query.parse(language, qs)
-  if node:type() == 'string' then
-    node = node:named_child()
-  end
-  if node:parent():parent():parent():type() == 'array_creation_expression' then
-    local class_node = node:parent():parent():parent():named_children()[1]:child():child()
-    local class_name = ts.get_node_text(class_node, 0)
-    if string.sub(class_name, 1, 1) == '\\' then
-      class_name = string.sub(class_name, 2)
+  if sibling_type == 'name' then
+    local imported_query = ts.query.parse(language, imported_name_qs)
+    parse_groups(imported_query, node, 'imported')
+    if targetClass ~= nil and targetMethod ~= nil then
+      return true, targetClass, targetMethod, 'imported'
     end
-    if class_node ~= nil then
-      local query = ts.query.parse(language, '(namespace_use_clause) @namespace_use_clause')
-      for _, namespace_node, _ in query:iter_captures(root, 0) do
-        if namespace_node ~= nil then
-          local namespace_name = ts.get_node_text(namespace_node, 0)
-          if string.find(namespace_name, class_name) then
-            return true, namespace_node, node
-          end
-        end
-      end
+  else
+    local qualified_name_query = ts.query.parse(language, qualified_name_qs)
+    parse_groups(qualified_name_query, node, 'inline')
+    if targetClass ~= nil and targetMethod ~= nil then
+      return true, targetClass, targetMethod, 'inline'
     end
   end
+
+
   return false
 end
 
 ---@return boolean
 M.go_to_definition = function()
-  local isValidTouple, class_node, method_node = M.validate_touple()
-  if isValidTouple and class_node ~= nil then
-    local method_name = ts.get_node_text(method_node, 0)
-    local class_name = ts.get_node_text(class_node, 0):gsub('\\', '/')
-    vim.cmd('find +/' .. method_name .. ' ' ..class_name)
+  local isValidTouple, className, methodName, type = M.validate_touple()
+  if isValidTouple and className and methodName then
+    className = get_namespace(className)
+    if className:sub(1, 1) == '\\' then
+      className = className:sub(2)
+    end
+    className = className:gsub('\\', '/')
+    vim.cmd('find +/' .. methodName .. ' ' .. className)
     vim.cmd('norm zzwww')
     return true
   end
@@ -68,4 +123,3 @@ end
 M.setup = function() end
 
 return M
-
