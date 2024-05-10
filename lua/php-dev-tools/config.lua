0ad4075f710f9b87c23a253cc9daa2f4b1aa11de
adaviloper <16654653+adaviloper@users.noreply.github.com>
2024-05-10 09:26:26 -0500

    resetting to just assign the whole config block

--- lua/php-dev-tools/config.lua
+++ lua/php-dev-tools/config.lua
@@ -4,9 +4,7 @@ local Config = {
 
 function Config.set(opts)
   for _, project_specs in ipairs(opts.test_utils) do
-    Config.test_utils[vim.fs.normalize(project_specs.dir)] = {
-      cmd = project_specs.cmd,
-    }
+    Config.test_utils[vim.fs.normalize(project_specs.dir)] = project_specs
   end
 end
 
